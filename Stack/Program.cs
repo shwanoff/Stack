9bc24b694373d9527768d5089fb0c32cb082d12d
Вадим Шванов <adm.shwan@gmail.com>
2018-03-22 19:31:00 +0300

    Исправлена ошибка при дублировании элементов

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Stack
 {
@@ -10,6 +6,31 @@ namespace Stack
     {
         static void Main(string[] args)
         {
+            // Создаем новый стек.
+            var stack = new Stack<int>();
+
+            // Добавляем новые элементы в стек.
+            stack.Push(1);
+            stack.Push(7);
+            stack.Push(42);
+            stack.Push(69);
+            stack.Push(-17);
+            Console.Write($"Стек содержит {stack.Count} элементов.");
+
+            // Получаем элементы с удалением.
+            var item1 = stack.Pop();
+            Console.WriteLine($"Верхний элемент {item1}.");
+            var item2 = stack.Pop();
+            Console.WriteLine($"Предпоследний элемент {item2}.");
+
+            // Получаем элемент без удаления.
+            stack.Push(88);
+            var item3 = stack.Peek();
+            Console.WriteLine($"Новый верхний элемент {item3}.");
+            var item4 = stack.Peek();
+            Console.WriteLine($"Новый верхний элемент {item4}.");
+
+            Console.ReadLine();
         }
     }
 }
---
+++ Stack/Stack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    /// <summary>
+    /// Стек
+    /// </summary>
+    /// <typeparam name="T"> Тип данных, хранимых в стеке. </typeparam>
+    public class Stack<T>
+    {
+        /// <summary>
+        /// Коллекция хранимых данных.
+        /// </summary>
+        private List<T> _items = new List<T>();
+
+        /// <summary>
+        /// Количество элементов.
+        /// </summary>
+        public int Count => _items.Count;
+
+        /// <summary>
+        /// Добавить данные в стек.
+        /// </summary>
+        /// <param name="item"> Добавляемые данные. </param>
+        public void Push(T item)
+        {
+            // Проверяем входные данные на пустоту.
+            if(item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            // Добавляем данные в коллекцию элементов.
+            _items.Add(item);
+        }
+
+        /// <summary>
+        /// Получить верхний элемент стека с удалением.
+        /// </summary>
+        /// <returns> Элемент данных. </returns>
+        public T Pop()
+        {
+            // Получаем верхний элемент.
+            var item = _items.LastOrDefault();
+
+            // Если элемент пуст, то сообщаем об ошибке.
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пуст. Нет элементов для получения.");
+            }
+
+            // Удаляем последний элемент из коллекции.
+            _items.RemoveAt(_items.Count - 1);
+
+            // Возвращаем полученный элемент.
+            return item;
+        }
+
+        /// <summary>
+        /// Прочитать верхний элемент стека без удаления.
+        /// </summary>
+        /// <returns> Элемент данных. </returns>
+        public T Peek()
+        {
+            // Получаем верхний элемент.
+            var item = _items.LastOrDefault();
+
+            // Если элемент пуст, то сообщаем об ошибке.
+            if (item == null)
+            {
+                throw new NullReferenceException("Стек пуст. Нет элементов для получения.");
+            }
+
+            return item;
+        }
+    }
+}
